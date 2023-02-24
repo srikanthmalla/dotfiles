a962c900c5e44f7607abd9e180f31ed18e57497f
srikanthmalla <mallasrikanth004@gmail.com>
2023-02-24 11:31:11 -0800

    color reference added

--- zsh-autosuggestions-config.zsh
+++ zsh-autosuggestions-config.zsh
@@ -1,2 +1,3 @@
 # ~/.oh-my-zsh/custom/zsh-autosuggestions-config.zsh
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=cyan'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#b2b2b2'
+#for colors referece: https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
